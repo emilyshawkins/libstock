c269a0df6397b131d191af9f7ecbe456adddcd8d
Emily Hawkins <emilyshawkins.5904@gmail.com>
2024-10-22 11:50:12 -0700

    Modify backend folder

--- libstock_backend/src/main/java/com/example/libstock_backend/LibstockBackendApplication.java
+++ libstock_backend/src/main/java/com/example/libstock_backend/LibstockBackendApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.libstock_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class LibstockBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(LibstockBackendApplication.class, args);
 	}
 
 }
