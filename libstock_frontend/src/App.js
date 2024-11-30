712f2ea663e81ffe17940a38c9218ba82216b957
Nhi Nguyen <70039953+Nhinguyen0506@users.noreply.github.com>
2024-11-29 21:31:51 -0800

    Add files via upload

--- libstock_frontend/src/App.js
+++ libstock_frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar';
+import SignInPage from './SignInPage';  // Import SignInPage component
+import SignUpPage from './SignUpPage';  // Import SignUpPage component
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />  {/* Home page */}
+                <Route path="/signin" element={<SignInPage />} />  {/* Sign In page */}
+                <Route path="/signup" element={<SignUpPage />} />  {/* Sign Up page */}
+            </Routes>
+        </Router>
+    );
+}
+
+function Home() {
+    return <h1>Welcome to LibStock</h1>;  // Placeholder home page
 }
 
 export default App;
