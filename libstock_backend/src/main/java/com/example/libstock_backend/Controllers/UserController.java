ba01f1b269acde9d19bcb46c8171d81aef47bc54
Emily Hawkins <emilyshawkins.5904@gmail.com>
2024-12-05 12:01:34 -0800

    Added update and delete endpoints for users

--- libstock_backend/src/main/java/com/example/libstock_backend/Controllers/UserController.java
+++ libstock_backend/src/main/java/com/example/libstock_backend/Controllers/UserController.java
@@ -4,10 +4,13 @@ import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.libstock_backend.DTOs.LoginDTO;
@@ -24,7 +27,7 @@ public class UserController {
     UserRepository userRepository;
 
     @PostMapping("/admin_signup")
-    public ResponseEntity<UserDTO> signup(@RequestBody User user) {
+    public ResponseEntity<UserDTO> create(@RequestBody User user) {
         User existingUser = userRepository.findByEmail(user.getEmail());
         if (existingUser != null) {
             return ResponseEntity.status(Response.SC_CONFLICT).body(null);
@@ -38,6 +41,15 @@ public class UserController {
         User existingUser = userRepository.findByEmail(user.getEmail());
         if (existingUser == null || !existingUser.getPassword().equals(user.getPassword())) {
             return ResponseEntity.status(Response.SC_UNAUTHORIZED).body(null);
+        }
+        return ResponseEntity.ok(new UserDTO(existingUser.getEmail(), existingUser.getFirstName(), existingUser.getLastName(), existingUser.isAdmin(), existingUser.getAddress()));
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<UserDTO> get(@RequestParam String email) {
+        User existingUser = userRepository.findByEmail(email);
+        if (existingUser == null) {
+            return ResponseEntity.status(Response.SC_NOT_FOUND).body(null);
         }
         return ResponseEntity.ok(new UserDTO(existingUser.getEmail(), existingUser.getFirstName(), existingUser.getLastName(), existingUser.isAdmin(), existingUser.getAddress()));
     }
@@ -80,5 +92,14 @@ public class UserController {
         return ResponseEntity.ok(new UserDTO(existingUser.getEmail(), existingUser.getFirstName(), existingUser.getLastName(), existingUser.isAdmin(), existingUser.getAddress()));
     }
     
+    @DeleteMapping("/delete")
+    public ResponseEntity<UserDTO> delete(@RequestBody User user) {
+        User existingUser = userRepository.findByEmail(user.getEmail());
+        if (existingUser == null) {
+            return ResponseEntity.status(Response.SC_NOT_FOUND).body(null);
+        }
+        userRepository.delete(existingUser);
+        return ResponseEntity.ok(new UserDTO(existingUser.getEmail(), existingUser.getFirstName(), existingUser.getLastName(), existingUser.isAdmin(), existingUser.getAddress()));
+    }
 
 }
