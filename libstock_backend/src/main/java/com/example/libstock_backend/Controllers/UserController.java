22da4841a12fc4ca462ce27cc7e2160c3a9d29bd
Emily Hawkins <emilyshawkins.5904@gmail.com>
2024-12-05 11:49:09 -0800

    Added update endpoint for user profile

--- libstock_backend/src/main/java/com/example/libstock_backend/Controllers/UserController.java
+++ libstock_backend/src/main/java/com/example/libstock_backend/Controllers/UserController.java
@@ -4,6 +4,7 @@ import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,44 @@ public class UserController {
         if (existingUser == null || !existingUser.getPassword().equals(user.getPassword())) {
             return ResponseEntity.status(Response.SC_UNAUTHORIZED).body(null);
         }
+        return ResponseEntity.ok(new UserDTO(existingUser.getEmail(), existingUser.getFirstName(), existingUser.getLastName(), existingUser.isAdmin(), existingUser.getAddress()));
+    }
+
+    @PatchMapping("/update")
+    public ResponseEntity<UserDTO> update(@RequestBody User user) {
+        User existingUser = userRepository.findByEmail(user.getEmail());
+        if (existingUser == null) {
+            return ResponseEntity.status(Response.SC_NOT_FOUND).body(null);
+        }
+
+        // Check if email is being changed and if so, check if it is already in use
+        if (user.getEmail() != null) {
+            if (!user.getEmail().equals(existingUser.getEmail())) {
+                User duplicateUser = userRepository.findByEmail(user.getEmail());
+                if (duplicateUser != null) {
+                    return ResponseEntity.status(Response.SC_CONFLICT).body(null);
+                }
+                existingUser.setEmail(user.getEmail());
+            }
+        }
+        if (user.getFirstName() != null) {
+            existingUser.setFirstName(user.getFirstName());
+        }
+        if (user.getLastName() != null) {
+            existingUser.setLastName(user.getLastName());
+        }
+        if (user.getPassword() != null) {
+            existingUser.setPassword(user.getPassword());
+        }
+        if (user.getAddress() != null) {
+            existingUser.setAddress(user.getAddress());
+        }
+
+        // Should not be appear as an option but if attempted should not be able to change admin status
+        if (user.isAdmin() != existingUser.isAdmin()) {
+            return ResponseEntity.status(Response.SC_FORBIDDEN).body(null);
+        }
+        userRepository.save(existingUser);
         return ResponseEntity.ok(new UserDTO(existingUser.getEmail(), existingUser.getFirstName(), existingUser.getLastName(), existingUser.isAdmin(), existingUser.getAddress()));
     }
     
