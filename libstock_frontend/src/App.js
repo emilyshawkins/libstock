090504846744c4e51b0bf0f28620db5c5f6d1c16
ATran2404 <112041892+ATran2404@users.noreply.github.com>
2024-12-11 22:28:57 -0800

    Add files via upload

--- libstock_frontend/src/App.js
+++ libstock_frontend/src/App.js
@@ -1,25 +1,46 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar';
-import SignInPage from './SignInPage';  // Import SignInPage component
-import SignUpPage from './SignUpPage';  // Import SignUpPage component
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import Navbar from './Navbar'; // Import Navbar
+import SignInPage from './SignInPage'; // Import SignInPage
+import SignUpPage from './SignUpPage'; // Import SignUpPage
+import UserHomePage from './UserHomePage'; // Import UserHomePage
 
 function App() {
     return (
         <Router>
-            <Navbar />
-            <Routes>
-                <Route path="/" element={<Home />} />  {/* Home page */}
-                <Route path="/signin" element={<SignInPage />} />  {/* Sign In page */}
-                <Route path="/signup" element={<SignUpPage />} />  {/* Sign Up page */}
-            </Routes>
+            <MainContent />
         </Router>
     );
 }
 
+function MainContent() {
+    const location = useLocation(); // Get the current route path
+
+    // Determine if Navbar should be displayed
+    const isNavbarVisible = location.pathname !== '/user/home';
+
+    return (
+        <>
+            {/* Display Navbar only if not on /user/home */}
+            {isNavbarVisible && <Navbar />}
+            <Routes>
+                <Route path="/" element={<Home />} /> {/* Home page */}
+                <Route path="/signin" element={<SignInPage />} /> {/* Sign In page */}
+                <Route path="/signup" element={<SignUpPage />} /> {/* Sign Up page */}
+                <Route path="/user/home" element={<UserHomePage />} /> {/* User Home page */}
+                <Route path="*" element={<NotFound />} /> {/* Catch-all route */}
+            </Routes>
+        </>
+    );
+}
+
 function Home() {
-    return <h1>Welcome to LibStock</h1>;  // Placeholder home page
+    return <h1>Welcome to LibStock</h1>; // Placeholder home page
+}
+
+function NotFound() {
+    return <h1>404 - Page Not Found</h1>; // Catch-all page
 }
 
 export default App;
