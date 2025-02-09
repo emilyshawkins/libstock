f0e71d03bf11d31878cd3ec309407f254b8ae7c1
Emily Hawkins <emilyshawkins.5904@gmail.com>
2025-02-08 17:45:15 -0800

    Added /favorites/get_favorites_by_user endpoint

--- libstock_backend/src/main/java/com/example/libstock_backend/Controllers/FavoriteController.java
+++ libstock_backend/src/main/java/com/example/libstock_backend/Controllers/FavoriteController.java
@@ -1,5 +1,8 @@
 package com.example.libstock_backend.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.libstock_backend.Models.Book;
 import com.example.libstock_backend.Models.Favorite;
+import com.example.libstock_backend.Repositories.BookRepository;
 import com.example.libstock_backend.Repositories.FavoriteRepository;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -21,6 +26,8 @@ public class FavoriteController {
 
     @Autowired
     FavoriteRepository favoriteRepository;
+    @Autowired
+    BookRepository bookRepository;
 
     @PostMapping("/create")
     public ResponseEntity<Favorite> create_favorite(@RequestBody Favorite favorite) {
@@ -59,6 +66,20 @@ public class FavoriteController {
         }
         favoriteRepository.delete(favorite);
         return ResponseEntity.ok(favorite);
+    }
+
+    @GetMapping("/get_favorites_by_user")
+    public ResponseEntity<Iterable<Book>> get_favorites_by_user(@RequestParam String userId) {
+        Iterable<Favorite> favorites = favoriteRepository.findByUserId(userId);
+        List<Book> books = new ArrayList<>();
+        for (Favorite favorite : favorites) {
+            Book book = bookRepository.findById(favorite.getBookId()).orElse(null);
+            if (book != null) {
+                books.add(book);
+            }
+        }
+
+        return ResponseEntity.ok(books);
     }
 
     
